8cc276afd1af214095d3819c52a6445836ddb65c
Abhilash Anil <2020.abhilash@gmail.com>
2016-08-31 21:41:51 -0400

    Updated w3styles

--- app/view2/view2.js
+++ app/view2/view2.js
@@ -12,8 +12,7 @@ angular.module('myApp.view2', ['ngRoute'])
 .controller('View2Ctrl', ['$scope', '$http', '$window', function($scope, $http, $window) {
   var url = 'https://data.gov.in/api/datastore/resource.json?resource_id=7eca2fa3-d6f5-444e-b3d6-faa441e35294&api-key=574cfe75dbb216592ad3419d97bfa16c&offset=' + '0&';
 
-  url = url + 'fields=StateName&';
-  url = url + 'filter=[StateName]=Karnataka';
+  url = url + 'fields=StateName';
 
   $http({
     method: 'GET',
